9ae6635d6eda1153d271a8d44378065aadc8fd9f
LincolnLink <link7-7@hotmail.com>
2021-03-26 21:48:26 -0300

    finalizando tela de cliente3

--- wkAngularProjeto/src/app/app-routing.module.ts
+++ wkAngularProjeto/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '', pathMatch: 'full', redirectTo: 'produto'
+    path: '', pathMatch: 'full', redirectTo: 'home'
    },
    {
      path: 'produto',
